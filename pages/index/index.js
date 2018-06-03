c3bc072e8ac84ec68c826d57072495c36baf2699
sbuteryd@qq.com <34436143+sbuteryd@users.noreply.github.com>
2018-06-03 17:33:18 +0800

    get api data

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,4 +11,15 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
+  onLoad(){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data:{
+        type:'gj'
+      },
+      success:function(res){
+        console.log(res.data)
+      }
+    })
+  }
 })
